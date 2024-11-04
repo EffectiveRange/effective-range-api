fa2f6e47f96a70e4a67759c227e9b9aec62adfc5
AttilaGombosER <attila.gombos@effective-range.com>
2024-11-04 08:24:47 +0100

    Delete .spectral.mjs

--- .spectral.mjs
+++
@@ -1,2 +0,0 @@
-import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI1OTI3OQ/spectral.js?branch_slug=main&token=e885638d-1df5-4fb3-8b4d-61f4ea04d989";
-export default { extends: ruleset };
